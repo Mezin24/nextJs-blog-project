b02e2f2f47120f82e7f30a407115593de739a046
Pavel Mezencev <mezencev24@mail.ru>
2022-11-09 16:01:57 +0300

    add hero section

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,7 +1,24 @@
-import './hero.module.css';
+import Image from 'next/image';
+import classes from './hero.module.css';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/max.png'
+          alt='Max photo'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am Max!</h1>
+      <p>
+        A blog about Web development - espetialy about frontend frameworks like
+        Angular or React
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
