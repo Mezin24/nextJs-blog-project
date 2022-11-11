a337d5ffc314eedb7a171445923de2aa8777b88b
Pavel Mezencev <mezencev24@mail.ru>
2022-11-11 15:56:21 +0300

    add markdown rendering in jsx posts components

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,14 +1,56 @@
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 const PostContent = ({ post }) => {
-  const imagePath = `/images/posts/${post.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt='image'
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,7 +2,6 @@ import PostContent from '../../components/posts/post-detail/post-content';
 import { getPostData, readFiles } from '../../lib/post-util';
 
 const SinglePostPage = ({ post }) => {
-  console.log(post);
   return <PostContent post={post} />;
 };
 
@@ -23,7 +22,6 @@ export const getStaticPaths = async () => {
   const paths = readFiles().map((file) => ({
     params: { slug: file.replace(/\.md$/, '') },
   }));
-  console.log(paths);
 
   return {
     paths,
